709e0c9c6b85519e83e48993b77890a81435d28d
chiro0114 <vqrichiro@gmail.com>
2023-12-19 00:33:05 +0900

    feat: リンクが機能するように修正

--- src/components/sidebar/SidebarItem.tsx
+++ src/components/sidebar/SidebarItem.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { twMerge } from 'tailwind-merge';
-import Button from '../Button';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
@@ -16,7 +15,7 @@ const SidebarItem: React.FC<Props> = ({ title, href }) => {
   return (
     <div>
       <Link
-        href=''
+        href={href}
         className={twMerge(
           `text-3xl py-4  mb-5 w-52 rounded-full tracking-wide block font-bold cursor-pointer text-center`,
           href === pathname && 'press-nueumorphism'
